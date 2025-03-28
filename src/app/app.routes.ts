18e019a3d6d0171debedc93cd0d2a027c7adaf5d
KaliGa70 <1andresga07@gmail.com>
2025-03-28 15:51:58 -0600

    add: login & register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,26 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/inbox',
+    redirectTo: 'auth/login',
     pathMatch: 'full',
   },
   {
     path: 'folder/:id',
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
+  },
+  {
+    path: 'auth',
+    children:[
+      {
+        path: 'login',
+        loadComponent: () => import('./auth/login/login.page').then( m => m.LoginPage)
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./auth/register/register.page').then( m => m.RegisterPage)
+      },
+    ]
+
   },
 ];
